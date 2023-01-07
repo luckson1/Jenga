5e29d3679b52a48a6b43354f49eb3db713334bf6
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-01-07 22:35:27 +0300

    style: better formatting

--- pages/components/display/SubCategory.tsx
+++ pages/components/display/SubCategory.tsx
@@ -9,7 +9,7 @@ export interface Category {
 const SubCategory = ({category}: {category:Category}) => {
    
   return (
-  <div className='h-24  w-36 md:h-28 md:w-44 lg:w-48 lg:h-40 rounded-md  bg-center flex flex-col justify-center items-start bg-white cursor-pointer shadow-md hover:shadow-xl pt-0 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105'>
+  <div className='h-36  w-36 md:h-44 md:w-44 lg:w-48 lg:h-48 rounded-md  bg-center flex flex-col justify-center items-start bg-white cursor-pointer shadow-md hover:shadow-xl pt-0 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105'>
       <div className=" relative h-[80%]  w-[100%] md:h-[80%] md:w-[100%] lg:w-[100%] lg:h-[80%] rounded-md  bg-center flex flex-col justify-center gap-10  "  >
    <Image alt= {`${category?.name}` } src={`${category?.image}`} fill className='rounded-t-md md mt-0'/>
        
