b23ece1fa212a060d75da77e97bef97a067e811a
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-01-21 03:22:53 +0300

    style: bettter shop style

--- src/components/display/CategoryDisplay.tsx
+++ src/components/display/CategoryDisplay.tsx
@@ -1,30 +1,33 @@
-import Image from 'next/image'
-import { useRouter } from 'next/router'
-import React from 'react'
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React from "react";
 export interface Category {
-    image: string,
-    name: string,
-    i: number
-    url:string
+  image: string;
+  name: string;
+  i: number;
+  url: string;
 }
 
-const CategoryDisplay = ({category}: {category:Category}) => {
-    const router= useRouter()
+const CategoryDisplay = ({ category }: { category: Category }) => {
+  const router = useRouter();
   return (
-    <div className= "h-64 w-64 lg:w-72 lg:h-72 rounded-md   bg-cover  bg-center flex justify-center items-center relative"  >
-         <Image
-         alt={category?.name}
-            src= {category?.image}
-          fill
-          sizes="(max-width: 288px) 288px,(max-height: 288px) 288px "
-        
-            quality={50}
-            className="rounded-2xl"
-        />
-        <button className='bg-violet-400 bg-opacity-70 text-violet-700 py-2 w-32 rounded-full outline outline-violet-700  hover:outline-none hover:bg-violet-700 hover:text-slate-100 z-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105' onClick={()=> router.push(category?.url)}>{category?.name}</button>
-
+    <div className="relative flex h-72 w-72  items-center justify-center rounded-md   bg-cover  bg-center md:h-64 md:w-64 lg:h-72 lg:w-72">
+      <Image
+        alt={category?.name}
+        src={category?.image}
+        fill
+        sizes="(max-width: 288px) 288px,(max-height: 288px) 288px "
+        quality={50}
+        className="rounded-2xl"
+      />
+      <button
+        className="z-10 w-32 transform rounded-full bg-violet-400 bg-opacity-70 py-2 text-violet-700  outline outline-violet-700 transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-violet-700 hover:text-slate-100 hover:outline-none"
+        onClick={() => router.push(category?.url)}
+      >
+        {category?.name}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryDisplay
+export default CategoryDisplay;
