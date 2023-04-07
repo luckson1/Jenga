1de11130519cce2e7093ef4f5c9751f9b5535484
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-04-07 16:56:31 +0300

    fix: better nav

--- src/components/navigation/Nav.tsx
+++ src/components/navigation/Nav.tsx
@@ -1,50 +1,58 @@
-import React from 'react'
-import { FaHouseUser} from 'react-icons/fa'
-import { AiOutlineShoppingCart} from 'react-icons/ai'
-import { CgProfile} from 'react-icons/cg'
-import { MdEngineering } from 'react-icons/md'
-import { useRouter } from 'next/router'
-import { signOut, useSession } from 'next-auth/react'
-import ProfileCard from '../display/ProfileCard'
-
-
+import React from "react";
+import { FaHouseUser } from "react-icons/fa";
+import { IoIosLogIn } from "react-icons/io";
+import { useRouter } from "next/router";
+import { signOut, useSession } from "next-auth/react";
+import ProfileCard from "../display/ProfileCard";
+import { NavMenu } from "./NavMenu";
+import { MdOutlineSell } from "react-icons/md";
 
 function Nav() {
-  const router=useRouter()
+  const router = useRouter();
   const { status, data } = useSession();
-  const userImage=data?.user?.image ?? " /kitche.jpg"
-  const userName=data?.user?.name ?? "Profile Pic"
+  const userImage = data?.user?.image ?? " /kitche.jpg";
+  const userName = data?.user?.name ?? "Profile Pic";
   const authenticated = status === "authenticated";
   const handleLogout = () => {
     signOut();
   };
   return (
-    <nav className=" fixed bottom-1 shadow-slate-500/100 md:top-0 z-30 md:mx-0  flex h-12 md:h-16   w-full flex-row justify-between  bg-base-200 px-3 ">
-     <div className='flex  my-auto h-10 md:h-12 flex-col md:flex-row transition duration-300 ease-in-out transform hover:-translate-x-1 hover:scale-105 align-baseline gap-0.5 md:gap-2  hover:bg-slate-50 hover:shadow cursor-pointer  px-1 md:p-2' onClick={()=> router.push("/")}>
-      <FaHouseUser className='text-lg  md:text-2xl   cursor-pointer  text-indigo-700' onClick={()=> router.push("/")}/>
-      <p className='hover:text-sky-500 text-xs font-thin md:text-base'>Home</p>
+    <nav className=" fixed  top-0 z-30 flex h-12  w-full flex-row justify-between   bg-base-200 px-6 shadow-slate-500/100 md:mx-0  md:h-16 md:px-12 ">
+      <div className="flex h-full w-fit items-center justify-center text-sm capitalize">
+        {" "}
+        <button className="inline-flex items-center justify-center gap-2 rounded-md border border-transparent py-2 px-3 text-sm font-semibold text-base-content transition-all hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2" onClick={() => router.push("/")}>
+          {" "}
+          <FaHouseUser className="h-4 w-4" /> Home
+        </button>
       </div>
-      <div className='flex my-auto flex-row px-4 justify-start md:justify-center lg:justify-end md:px-8 gap-6 md:gap-16  w-[60%]'>
-        <div className='flex  flex-col md:flex-row transition duration-300 ease-in-out transform hover:-translate-x-1 hover:scale-105 align-baseline gap-0.5 md:gap-2  hover:bg-slate-50 hover:shadow cursor-pointer py-0.5 px-1 md:p-2' onClick={()=> router.push("/departments")}>
-<AiOutlineShoppingCart className='text-lg  md:text-2xl text-indigo-700'/>
-<p className='hover:text-sky-500 text-xs font-thin md:text-base'>Shop</p>
-        </div>
-        <div className='flex  flex-col md:flex-row transition duration-300 ease-in-out transform hover:-translate-x-1 hover:scale-105 align-baseline gap-0.5 md:gap-2  hover:bg-slate-50 hover:shadow cursor-pointer py-0.5 px-1 md:p-2' onClick={authenticated? ()=>router.push("/dashboard/onboarding"): ()=> router.push("/vendorAuth")}>
-<MdEngineering className='text-lg md:text-2xl text-indigo-700'/>
-<p className='hover:text-sky-500 text-xs font-thin md:text-base'>Become a Vendor</p>
-        </div>
-     
-
+      <div className="md:w-[40%]flex-row my-auto flex w-[20%] justify-start gap-3 md:justify-center md:gap-6   lg:justify-end">
+        <NavMenu />
       </div>
-      <div className='flex my-auto flex-row  justify-center  w-[20%]'>
-      <div className='flex  flex-col md:flex-row transition duration-300 ease-in-out transform hover:-translate-x-1 hover:scale-105 align-baseline gap-0.5 md:gap-2  hover:bg-slate-50 hover:shadow cursor-pointer py-0.5 px-1 md:p-2' onClick={authenticated? ()=> signOut(): ()=> router.push("/auth")}>
-{authenticated? <ProfileCard src={userImage} alt={userName}/> : <CgProfile className='text-lg md:text-2xl text-indigo-700'/>}
-<p className='hover:text-sky-500 text-xs font-thin md:text-base'>{authenticated? "Logout": "Login"}</p>
-        </div>
+      <div className="my-auto flex w-[50%] flex-row justify-center  gap-4 pr-5 md:w-[30%]">
+        <button
+          className="inline-flex items-center justify-center gap-2 rounded-md border border-transparent py-2 px-3 text-sm font-semibold text-base-content transition-all hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+          onClick={
+            authenticated
+              ? () => router.push("/dashboard/onboarding")
+              : () => router.push("/vendorAuth")
+          }
+        >
+          <MdOutlineSell className="h-4 w-4" /> Sell
+        </button>
+        <button
+          className="inline-flex items-center justify-center gap-2 rounded-md border border-transparent py-2 px-3 text-sm font-semibold text-blue-500 transition-all hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+          onClick={authenticated ? () => signOut() : () => router.push("/auth")}
+        >
+          {authenticated ? (
+            <ProfileCard src={userImage} alt={userName} />
+          ) : (
+            <IoIosLogIn className="h-4 w-4" />
+          )}{" "}
+          {authenticated ? "Logout" : "Login"}
+        </button>
       </div>
-
     </nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
