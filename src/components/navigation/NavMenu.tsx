5c959febd207e79180a47054664de4146079c321
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-04-07 18:52:36 +0300

    fix: better nav UI

--- src/components/navigation/NavMenu.tsx
+++ src/components/navigation/NavMenu.tsx
@@ -21,7 +21,7 @@ const NavigationMenu = React.forwardRef<
   <NavigationMenuPrimitive.Root
     ref={ref}
     className={cn(
-      "relative z-10 flex flex-1 items-center justify-center",
+      "relative z-10 flex flex-1 items-center justify-center -ml-20 md:ml-0 ",
       className
     )}
     {...props}
@@ -148,7 +148,7 @@ export  function NavMenu() {
         <NavigationMenuItem>
           <NavigationMenuTrigger>Shop</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+            <ul className="grid w-[380px]  gap-3 p-2 md:w-[500px] grid-cols-2 lg:w-[600px] overflow-scroll text-xs md:text-sm">
               {departments && departments.map(department=> (
                      <ListItem
                      key={department.id}
@@ -161,7 +161,6 @@ export  function NavMenu() {
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
-      
       
       </NavigationMenuList>
     </NavigationMenu>
