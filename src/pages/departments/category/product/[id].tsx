d02b6aee4d2da6269940f511615db1fe99838bf8
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-04-10 03:35:58 +0300

    fix: Better UI

--- src/pages/departments/category/product/[id].tsx
+++ src/pages/departments/category/product/[id].tsx
@@ -4,10 +4,9 @@ import { api } from "../../../../utils/api";
 import Image from "next/image";
 import { BsWhatsapp } from "react-icons/bs";
 import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Product, User } from "@prisma/client";
 import { GetStaticProps } from "next";
 import Head from "next/head";
-
 
 const prisma = new PrismaClient();
 
@@ -26,200 +25,286 @@ export async function getStaticPaths() {
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const productId = context?.params?.id as string;
-  const p= await prisma.product.findUniqueOrThrow({
+  const p = await prisma.product.findUniqueOrThrow({
     where: {
-      id: productId
+      id: productId,
     },
     select: {
       name: true,
-      id: true
-    }
-  })
+      id: true,
+    },
+  });
 
-
-  return { props: { p} };
+  return { props: { p } };
 };
 
-
-const ProductId = ({p}: {p: {
-  id: string;
-  name: string;
-}}) => {
-
-  const [currentImage, setCurrentImage] = useState("/livingRoom.jpeg");
-
-
-  const { data: images} = api.image.imagesAndUser.useQuery({
-  productId:p.id
-  }, {
-onSuccess: (images)=> setCurrentImage(images.at(0)?.url ?? "/livingRoom.jpeg")
-  });
- 
+const ProductId = ({
+  p,
+}: {
+  p: {
+    id: string;
+    name: string;
+  };
+}) => {
+  const { data: images } = api.image.imagesAndUser.useQuery(
+    {
+      productId: p.id,
+    },
+    {
+      onSuccess: (images) => setCurrentImage(images.at(0)),
+    }
+  );
+  type Images = {
+    url: string;
+    id: string;
+    productId: string;
+    userId: string;
+    deleted: boolean;
+    user: User;
+    Product: Product;
+  };
+  const [currentImage, setCurrentImage] = useState<Images>();
+  enum DisplayTypes {
+    "product",
+    "seller",
+    "delivery",
+  }
+  const [display, setDisplay] = useState<DisplayTypes>(DisplayTypes.product);
   const product = images?.at(0)?.Product;
   const user = images?.at(0)?.user;
 
-
   return (
     <>
-    
-    <Head>
+      <Head>
         <title>Jenga</title>
         <meta name="description" content={p.name} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-    <div className=" flex flex-col my-16">
-      <div className="flex h-[75vh] w-screen flex-col ">
-        <div className=" flex h-full  w-full flex-col justify-center md:h-[90%]  md:flex-row  ">
-          <div className="flex h-[50%] w-full flex-col-reverse items-start justify-around md:h-[70%] md:w-[60%] md:flex-row lg:h-full lg:w-[50%]">
-            <div className="relative mx-5 flex h-fit w-screen flex-row gap-2 p-1 md:ml-8 md:h-screen md:w-fit md:flex-col">
-              {
-                images?.map((image) => (
-                  <div
-                    key={image.id}
-                    className="relative flex h-12 w-12 cursor-pointer items-center justify-center overflow-scroll rounded-lg md:h-16 md:w-16"
-                    onClick={() => setCurrentImage(image.url)}
-                  >
-                    <Image
+      <div className=" my-16 flex flex-col gap-10">
+        <div className="flex h-screen w-screen flex-col md:h-[75vh] ">
+          <div className=" flex h-full  w-full flex-col justify-center md:h-[90%]  md:flex-row  ">
+            <div className="flex h-[50%] w-full flex-col-reverse items-start justify-around  md:h-full md:w-[50%] md:flex-row">
+              <div className="relative mx-5 flex h-[20%] w-full flex-row items-center justify-center gap-2 p-1 md:ml-8 md:h-full md:w-[20%] md:flex-col">
+                {images &&
+                  images?.map((image) => (
+                    <div
                       key={image.id}
-                      src={image.url}
-                      alt={image.Product.name}
-                      fill
-                      sizes="(max-width: 768px) 40px,
+                      className={`relative flex h-12 w-12 cursor-pointer items-center justify-center overflow-scroll rounded-lg md:h-16 md:w-16 ${
+                        currentImage?.id === image.id
+                          ? "outline outline-2 outline-primary"
+                          : ""
+                      }`}
+                      onClick={() => setCurrentImage(image)}
+                    >
+                      <Image
+                        key={image.id}
+                        src={image.url}
+                        alt={image.Product.name}
+                        fill
+                        sizes="(max-width: 768px) 40px,
               (max-width: 1200px) 50px,
               50px"
-                      className="rounded-lg"
-                    />
-                  </div>
-                ))
-              }
-            </div>
-            <div className="relative mx-auto h-[80%] w-[95%] rounded-lg md:h-[95%] md:w-[80%]">
-              <Image
-                src={currentImage }
-                alt={product?.name ?? " Product"}
-                quality={100}
-                fill
-                sizes="(max-width: 768px) 80vw,
+                        className="rounded-lg"
+                      />
+                    </div>
+                  ))}
+              </div>
+              <div className=" relative mx-auto  flex  h-[80%]  w-full max-w-xl rounded-lg  md:shadow-lg shadow-base-content md:my-auto md:w-[80%]">
+                <div className="relative  h-full w-full">
+                  <Image
+                    src={currentImage?.url ?? "/livingRoom.jpeg"}
+                    alt={product?.name ?? " Product"}
+                    quality={100}
+                    fill
+                    sizes="(max-width: 768px) 80vw,
               (max-width: 1200px) 80vw,
               80vw"
-                className="rounded-lg"
-              />
-            </div>
-          </div>
-          <div className="mx-auto flex h-[50%] w-full items-center justify-center md:h-full md:w-[40%] md:items-start lg:w-[50%] lg:items-center">
-            <div className="flex h-[80%] w-[95%] flex-col items-center justify-center rounded-lg bg-slate-50 p-5 shadow-lg shadow-slate-600/100 bg-opacity-10 md:h-[65%] md:w-[90%] md:p-10 lg:w-[80%] lg:h-[90%]">
-              <p className="text-2xl tracking-widest text-blue-700">
-                {product?.name}
-              </p>
-              <p className="text-xl font-bold tracking-widest">
-                Ksh. {product?.price}
-              </p>
-              <p className="text-xl tracking-widest ">
-                Condition: {product?.secondHand ? "Second Hand" : "New"}
-              </p>
-              <div className="flex h-20 w-full flex-row items-center justify-start gap-24 text-5xl md:gap-36">
-                <BsWhatsapp className="cursor-pointer text-green-500 shadow-green-500/50 " />{" "}
-                <AiOutlinePhone className="cursor-pointer text-sky-500 " />{" "}
-                <AiOutlineMail className="cursor-pointer text-violet-500" />
+                    className="rounded-b-lg sm:rounded-lg"
+                  />
+                </div>
+                {currentImage && images && (
+                  <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                    {/* provide logic of displaying the carousel images */}
+                    <button
+                      className="btn-circle btn bg-base-100 bg-opacity-30 text-xl text-slate-900"
+                      onClick={() =>
+                        images?.indexOf(currentImage) === 0
+                          ? setCurrentImage(images[images?.length - 1])
+                          : setCurrentImage(
+                              images?.at(images.indexOf(currentImage) - 1)
+                            )
+                      }
+                    >
+                      ❮
+                    </button>
+                    <button
+                      className="btn-circle btn bg-base-100 bg-opacity-30 text-xl text-slate-900"
+                      onClick={() =>
+                        images?.indexOf(currentImage) === images.length - 1
+                          ? setCurrentImage(images[0])
+                          : setCurrentImage(
+                              images[images.indexOf(currentImage) + 1]
+                            )
+                      }
+                    >
+                      ❯
+                    </button>
+                  </div>
+                )}
               </div>
-              <p className="text-xl tracking-widest ">
-                Location: {product?.location}
-              </p>
-
-              <p className="text-xl tracking-widest">By: {user?.name}</p>
+            </div>
+            <div className="mx-auto flex h-[50%] w-full items-center justify-center md:h-full md:w-[50%] md:items-start lg:w-[50%] lg:items-center">
+              <div className="card mx-auto my-auto h-full   w-full max-w-xl rounded-lg bg-base-100 md:shadow-lg shadow-base-content md:h-[80%] md:w-[80%] ">
+                <div className="card-body h-full w-full">
+                  <p className="text-2xl tracking-widest text-blue-700">
+                    {product?.name}
+                  </p>
+                  <p className="text-xl font-bold tracking-widest">
+                    Ksh. {product?.price}
+                  </p>
+                  <p className="text-xl tracking-widest ">
+                    Condition: {product?.secondHand ? "Ex-UK" : "New"}
+                  </p>
+                  <button
+                    type="button"
+                    className="inline-flex w-full items-center justify-center gap-2 rounded-md border border-transparent bg-green-500 py-3 px-4 text-sm font-semibold text-white transition-all hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800"
+                  >
+                    <BsWhatsapp className="h-6 w-6" /> Whatsapp
+                  </button>
+                  <button
+                    type="button"
+                    className="inline-flex w-full items-center justify-center gap-2 rounded-md border border-transparent bg-blue-500 py-3 px-4 text-sm font-semibold text-white transition-all hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800"
+                  >
+                    <AiOutlinePhone className="h-6 w-6 " /> Call
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="mx-auto mb-16 flex h-fit w-[95%] flex-col items-center  justify-start gap-5  md:flex-row">
-        <div
-          tabIndex={0}
-          className="collapse-arrow rounded-box collapse border border-base-300 bg-base-100 shadow shadow-violet-500/50"
-        >
-          <div className="collapse-title text-xl font-medium">
-            Product Details
+        <div className="mx-auto mb-16 flex h-fit w-[90%] flex-col  justify-start  gap-5 ">
+          <div className="tabs w-full">
+            <button
+              onClick={() => setDisplay(DisplayTypes.product)}
+              className={`tab tab-lifted ${
+                display === DisplayTypes.product ? "tab-active" : ""
+              }`}
+            >
+              Product 
+            </button>
+            <button
+              onClick={() => setDisplay(DisplayTypes.seller)}
+              className={`tab tab-lifted ${
+                display === DisplayTypes.seller ? "tab-active" : ""
+              }`}
+            >
+              Seller
+            </button>
+            <button
+              onClick={() => setDisplay(DisplayTypes.delivery)}
+              className={`tab tab-lifted ${
+                display === DisplayTypes.delivery ? "tab-active" : ""
+              }`}
+            >
+              Delivery 
+            </button>
           </div>
-          <div className="collapse-content flex flex-col">
-            <div className="mt-2 flex flex-row">
-              <div className="w-6/12 bg-slate-200 p-3 outline outline-slate-600">
-                Product Description
-              </div>
-              <div className="w-6/12 p-3 outline outline-slate-600">
+          {display === DisplayTypes.product && (
+            <div className=" flex flex-col gap-5">
+              <div className="mt-2 flex flex-col gap-5">
+                <p className="h2 text-lg font-bold"> Product Description </p>
+
                 <p> {product?.description}</p>
               </div>
-            </div>
-            {product?.sizes.length !== 0 && (
-              <div className="flex flex-row">
-                <div className="w-6/12 bg-slate-200 p-3 outline outline-slate-600">
-                  Product sizes
-                </div>
-                <div className="w-6/12 p-3 outline outline-slate-600">
+              {product?.sizes.length !== 0 && (
+                <div className="flex flex-row gap-5">
+                  <p className=" font-bold">Product sizes:</p>
+
                   {product?.sizes.map((size, index) => (
-                    <p key={index}>{size}</p>
+                    <p key={index}>{size}, </p>
                   ))}
                 </div>
-              </div>
-            )}
-            {product?.materials.length !== 0 && (
-              <div className="flex flex-row">
-                <div className="w-6/12 bg-slate-200 p-3 outline outline-slate-600">
-                  Product materials
-                </div>
-                <div className="w-6/12 p-3 outline outline-slate-600">
+              )}
+              {product?.materials.length !== 0 && (
+                <div className="flex flex-row gap-5">
+                  <p className=" font-bold">Product materials:</p>
+
                   {product?.materials.map((material, index) => (
-                    <p key={index}>{material}</p>
+                    <p key={index}>{material}, </p>
                   ))}
                 </div>
-              </div>
-            )}
-            {product?.colors.length !== 0 && (
-              <div className="flex flex-row">
-                <div className="w-6/12 bg-slate-200 p-3 outline outline-slate-600">
-                  Product colors
-                </div>
-                <div className="w-6/12 p-3 outline outline-slate-600">
+              )}
+              {product?.colors.length !== 0 && (
+                <div className="flex flex-row gap-5">
+                  <p className=" font-bold">Product colors:</p>
+
                   {product?.colors.map((color, index) => (
                     <span key={index}>{color}, </span>
                   ))}
                   {product?.width && (
-                    <div className="flex flex-row">
-                      <div className="w-6/12 bg-slate-200 p-3 outline outline-slate-600">
-                        Product width
-                      </div>
-                      <div className="w-6/12 p-3 outline outline-slate-600">
-                        <p> {product?.width}</p>
-                      </div>
+                    <div className="flex flex-row gap-5">
+                      <p className=" font-bold">Product width:</p>
+
+                      <p> {product?.width}</p>
                     </div>
                   )}
 
                   {product?.length && (
-                    <div className="flex flex-row">
-                      <div className="w-6/12 bg-slate-200 p-3 outline outline-slate-600">
-                        Product length
-                      </div>
-                      <div className="w-6/12 p-3 outline outline-slate-600">
-                        <p> {product?.length}</p>
-                      </div>
+                    <div className="flex flex-row gap-5">
+                      <p className=" font-bold">Product length:</p>
+
+                      <p> {product?.length}</p>
                     </div>
                   )}
                   {product?.height && (
-                    <div className="flex flex-row">
-                      <div className="w-6/12 bg-slate-200 p-3 outline outline-slate-600">
-                        Product height
-                      </div>
-                      <div className="w-6/12 p-3 outline outline-slate-600">
-                        <p> {product?.height}</p>
-                      </div>
+                    <div className="flex flex-row gap-5">
+                      <p className=" font-bold">Product height:</p>
+
+                      <p> {product?.height}</p>
                     </div>
                   )}
                 </div>
-              </div>
-            )}
-          </div>
+              )}
+            </div>
+          )}
+                {display === DisplayTypes.seller &&   <div className=" flex flex-col gap-5">
+                <div className="flex flex-row gap-5">
+                      <p className=" font-bold">Business Name:</p>
+
+                      <p>   {user?.businessName}</p>
+                    </div>
+                    <div className="flex flex-row gap-5">
+                      <p className=" font-bold">Business Location:</p>
+
+                      <p>    {user?.location}</p>
+                    </div>
+                    <div className="flex flex-row gap-5">
+                      <p className=" font-bold">Business Email:</p>
+
+                      <p>    {user?.website}</p>
+                    </div>
+                
+                 
+                 
+                  </div>}
+
+                  {display === DisplayTypes.delivery &&   <div className=" flex flex-col gap-5">
+                <div className="flex flex-row gap-5">
+                      <p className=" font-bold">Delivery Information:</p>
+
+                      <p>  Latest delivery information available on the retailer website <a  href={user?.website ?? "/"}
+            target="_blank"
+            rel="noreferrer nofollow" className="underline text-sky-500">here</a></p>
+                    </div>
+                 
+                   
+                
+                 
+                 
+                  </div>}
         </div>
       </div>
-    </div>
     </>
   );
 };
